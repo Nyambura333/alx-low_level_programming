f2763ff1de70f9c045c9d89d3ef04abac2003942
nyambura333 <stellanyambura26@gmail.com>
2024-07-06 01:41:22 +0300

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Make the alphebet
+ * Return:void
+ */
+
+void print_alphebet(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/print_alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+void print_alphabet(void)
+{
+	for (char ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+}
+
